c9efe83bded3b11e58365fa3860684c2ea2e8d1f
kemi Chris <ekeminichristopher123@gmail.com>
2024-06-13 02:36:10 +0100

    admin styles

--- src/components/AddWorkers.jsx
+++ src/components/AddWorkers.jsx
@@ -4,18 +4,17 @@ export const AddWorkers = () => {
     return (
         <div className='addWorkers'>
             <h2>Add Workers</h2>
-            <div className="addWorkers">
-                <form action="">
-                    <label htmlFor="address">Address</label>
-                    <input type="text" name="address" id="address" required />
-                    <label htmlFor="name">Address</label>
-                    <input type="text" name="name" id="name" required />
-                    <label htmlFor="id">Address</label>
-                    <input type="text" name="id" id="id" required />
 
-                    <button>Add</button>
-                </form>
-            </div>
+            <form className='form' action="">
+                <label htmlFor="address">Address</label>
+                <input type="text" name="address" id="address" required />
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" required />
+                <label htmlFor="id">Address</label>
+                <input type="text" name="id" id="id" required />
+
+                <button className='addBtn'>Add</button>
+            </form>
         </div>
     )
 }
--- src/components/RemoveWorkers.jsx
+++ src/components/RemoveWorkers.jsx
@@ -4,7 +4,7 @@ export const RemoveWorkers = () => {
     return (
         <div className="removeWorkers">
             <h2>Remove Workers</h2>
-            <form action="">
+            <form className="form" action="">
                 <label htmlFor="address">Address</label>
                 <input type="text" name="address" id="address" required />
 
