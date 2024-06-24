84c429a6b3857cf8f3b59115529ded621674a941
kemi Chris <ekeminichristopher123@gmail.com>
2024-06-24 16:53:25 +0100

    get workers list

--- src/components/WorkersList.jsx
+++ src/components/WorkersList.jsx
@@ -1,37 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import contractAbi from "../contract/contractABI.json"
+import React, { useState } from "react";
+import contractAbi from "../contract/contractABI.json";
 
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
 export const WorkersList = () => {
-    
     const contractAddress = "0x3541AAC732025c6df90faCd5aA9fe7EC397f23Ab";
-
 
     const [workers, setWorkers] = useState([]);
 
-    useEffect(() => {
-        const fetchWorkers = async () => {
+    const fetchWorkers = async () => {
+        try {
             const provider = new ethers.BrowserProvider(window.ethereum);
             await provider.send("eth_requestAccounts", []);
             const signer = await provider.getSigner();
-            const logBookContract = new ethers.Contract(contractAddress, contractAbi, signer);
+            const logBookContract = new ethers.Contract(
+                contractAddress,
+                contractAbi,
+                signer
+            );
 
-            const workersList = await logBookContract.workersList();
-            const workersData = workersList.map((worker) => {
-                return {
-                    address: worker.workerAddress,
-                    name: worker.name,
-                    id: worker.Id.toString(),
-                };
-            });
-            setWorkers(workersData);
-        };
-        fetchWorkers();
-    }, []);
+            const transaction = await logBookContract.getWorkersList();
+
+            let receipt;
+            receipt = await transaction;
+            console.log(receipt);
+
+            // const name = receipt[0] !== undefined ? receipt[0] : "";
+            // const id = receipt[1] !== undefined ? receipt[1].toString() : "";
+            // const signed = receipt[2] !== undefined ? receipt[2] : false;
+
+            // const [name, id, signed] = receipt;
+
+            // setWorkerName(name);
+            // setWorkerId(id);
+            // setSignedIn(signed);
+
+            // console.log(`Worker details: ${name}, ${id}, ${signed}`);
+
+            setWorkers(receipt);
+        } catch (error) {
+            console.log("Error getting workers data:", error);
+        }
+    };
 
     return (
-        <div className='workersList'>
+        <div className="workersList">
             <h2>Workers List</h2>
             <table>
                 <thead>
@@ -39,19 +52,23 @@ export const WorkersList = () => {
                         <th>Address</th>
                         <th>Name</th>
                         <th>ID</th>
+                        <th>Signed</th>
                     </tr>
                 </thead>
 
                 <tbody>
                     {workers.map((worker, index) => (
                         <tr key={index}>
-                            <td>{worker.address}</td>
+                            <td>{worker.workerAddress}</td>
                             <td>{worker.name}</td>
-                            <td>{("link unvailaible")}</td>
+                            <td>{worker.id}</td>
+                            <td>{worker.signedIn? "Yes": "No"}</td>
                         </tr>
                     ))}
                 </tbody>
+
+                <button onClick={fetchWorkers}></button>
             </table>
         </div>
-    )
-}
+    );
+};
