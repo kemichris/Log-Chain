7760016da37a5a1286077cf3906448c59fb5aff2
kemi Chris <ekeminichristopher123@gmail.com>
2024-06-24 19:52:00 +0100

    signed in workers contract integration

--- src/components/SignedInWorkers.jsx
+++ src/components/SignedInWorkers.jsx
@@ -1,28 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import contractAbi from "../contract/contractABI.json";
+
+import { ethers } from "ethers";
 
 export const SignedInWorkers = () => {
-  return (
-    <div className='signedInWorkers'>
-        <h2>Signed in workers</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Address</th>
-                    <th>Name</th>
-                    <th>ID</th>
-                    <th>Time</th>
-                </tr>
-            </thead>
+    const contractAddress = "0x68B3893355F6592b2C430d8bccAc495071639d43";
+    const [workers, setWorkers] = useState([]);
 
-            <tbody>
-                <tr>
-                    <td>....</td>
-                    <td>dfsfsd</td>
-                    <td>dsdfd</td>
-                    <td>11:50pm</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-  )
+
+    const fetchSignedWorkers = async () => {
+        try {
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            await provider.send("eth_requestAccounts", []);
+            const signer = await provider.getSigner();
+            const logBookContract = new ethers.Contract(
+                contractAddress,
+                contractAbi,
+                signer
+            );
+
+            const transaction = await logBookContract.getSignedInWorkers();
+            let receipt;
+            receipt = await transaction;
+            
+            setWorkers(receipt);
+        } catch (error) {
+            console.log("Error getting workers data:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchSignedWorkers()
+    })
+
+    return (
+        <div className='signedInWorkers'>
+            <h2>Signed in workers</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Address</th>
+                        <th>Name</th>
+                        <th>ID</th>
+                        {/* <th>Time</th> */}
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {workers.map((worker, index) => (
+                        <tr key={index}>
+                            <td>{worker.workerAddress}</td>
+                            <td>{worker.name}</td>
+                            <td>{(worker.Id).toString()}</td>
+                            {/* <td>{}</td> */}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
